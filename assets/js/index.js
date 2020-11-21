ffb67946f1b9185574eeac90d1f9dd733a81fca0
YounesHarrat <harratanis@hotmail.fr>
2020-11-21 15:37:47 +0100

    added dark theme toggler
    when toggle is on, document load a darkTheme.css file
    when it's off, there's a link element with href=""

--- assets/js/index.js
+++ assets/js/index.js
@@ -136,23 +136,25 @@ let init = ()=>{
 }
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.querySelector("#myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.querySelector("#myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.querySelector(".close");
+
+
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.addEventListener('click',()=> {
   modal.style.display = "block";
-}
+})
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.addEventListener('click',()=> {
   modal.style.display = "none";
-}
+})
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -164,3 +166,14 @@ window.onclick = function(event) {
 init()
 
 
+//Dark theme 
+
+let checkTheme = document.querySelector('#darkTheme')
+
+checkTheme.addEventListener('click',()=>{
+    if(checkTheme.checked){
+        document.querySelector('#linkAnchor').nextElementSibling.setAttribute("href","./assets/css/darkTheme.css")
+    } else {
+        document.querySelector('#linkAnchor').nextElementSibling.setAttribute("href","")
+    }
+})
